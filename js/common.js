0849759b5360f66aa787ac13a415c34256b3689e
Zoe-Kim <thfud333@gmail.com>
2021-06-28 22:33:17 +0900

    마우스 커서 이벤트 적용
    210628 10:33pm

--- js/common.js
+++ js/common.js
@@ -12,5 +12,21 @@ $(document).ready(function() {
   });
 
 
+  // 마우스 커서 따라다니는 원 이벤트
+  $('#wrap').on('mousemove', function (e) {
+    const mouseX = e.pageX;
+    const mouseY = e.pageY;
+    console.log(mouseX, mouseY);
+    /* 
+    <애니메이트를 대신하는 플러그인>
+    gsap.to('#box', {rotation: 27, x: 100, duration: 1});
+    첫 번째 파라미터는 트윈 할 대상(Target)
+    두 번째 속성(Properties)
+    시작 속성에서 끝 속성으로 변화가 발생
+    */
+    gsap.to('#circle', {left: mouseX-28, top: mouseY-26, duration: 0.2});
+});
+
+
 
 });
