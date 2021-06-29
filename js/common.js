da0ad0afe8f785f49035db2d66af22853fa1c752
Zoe-Kim <thfud333@gmail.com>
2021-06-29 14:33:56 +0900

    포커스 이동 오류 해결
    210629 02:31pm
    - .mGnb_close_btn은 보기에는 .mGnb_top에 위치하는 것 같지만, 실제로는 가장 하단의 데이터로 존재해야 하기 때문에 .mGnb 아래에 마크업 후, position: absolute 값으로 위치를 설정한다

--- js/common.js
+++ js/common.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
     else $('#m_head').removeClass('on');
   });
 
-  // .mGnb_wrap 열기
+  // .mGnb_wrap 열고 닫기
   const $mGnbwrap = $('#m_head .mGnb_wrap');
   $('#m_head .mGnb_open_btn').on('click', function () {
     const $first =  $mGnbwrap.find('.first');
@@ -33,23 +33,22 @@ $(document).ready(function() {
     const $openBtn = $(this);
 
     // .mGnb_dim 생성 후 fadeTo로 서서히 나타내기
-    $openBtn.after('<div class="mGnb_dim"></div>').next().stop().fadeTo('fast', 0.6);
-    $mgnbWrap.css('visibility', 'visible').stop().animate({left: 0}, 300, function () {
-      $first.focus();  //포커스 아웃라인 안보인다
-      $('html').css({overflowY: 'hidden', height: '100%'});
+    $openBtn.after('<div class="mGnb_dim"></div>').next().stop().fadeTo('fast', 0.7);
+    // .mGnb_wrap 우측에서 등장 애니메이션
+    $mGnbwrap.css('visibility', 'visible').stop().animate({right: 0}, 300, function () {
+      $first.focus(); // 상단 로고로 포커스 강제 이동
+      $('html').css({overflowY: 'hidden', height: '100%'}); // .mGnb_wrap 내 스크롤바 없애기
     });
 
-    //키보드 제어 접근성 - keydown
-    //$first에서 shift+tab 눌러 이전으로 나가는 경우 마지막으로 포커스 이동
+    // .mGnb_wrap 내 키보드 제어 접근성
+    // 1) $first에서 shift+tab 눌러 이전으로 나가는 경우, $last로 포커스 이동
     $first.on('keydown', function (e) {
-      //console.log(e.keyCode);  //tab => 9
-      if ( e.shiftKey && e.keyCode === 9 ) {
+      if (e.shiftKey && e.keyCode === 9) {
         e.preventDefault();
         $last.focus();
       }
     })
-
-    //$last에서 (shift는 누르면 안됨)tab만 눌러 다음으로 나가는 경우 처음으로 포커스 이동
+    // 2) $last에서 (shift 없이)tab만 눌러 다음으로 나가는 경우, $first로 포커스 이동
     $last.on('keydown', function (e) {
       if (!e.shiftKey && e.keyCode === 9) {
         e.preventDefault();
@@ -57,18 +56,17 @@ $(document).ready(function() {
       }
     });
 
-    //닫기 버튼 클릭으로 닫아주기 : animate() -> 완료함수 none, 열기버튼으로 포커스 강제 이동
-    $mgnbWrap.find('.btn_gnb_close').on('click', function () {
-      $('html').removeAttr('style');
-      $('#mgnbDim').stop().fadeOut('fast', function () {
-        $(this).remove();
+    // .mGnb_wrap 닫기
+    $mGnbwrap.find($last).on('click', function () {
+      $('html').removeAttr('style'); // 화면에 스크롤바 다시 만들기
+      $('.mGnb_dim').stop().fadeTo('slow', 0, function () {
+        $(this).remove(); // .mGnb_dim을 fadeTo로 서서히 없앤 뒤, 완전히 제거
       });
 
-      $mgnbWrap.stop().animate({left: '-100%'}, 300, function () {
+      // .mGnb_wrap 우측으로 사라지는 애니메이션
+      $mGnbwrap.stop().animate({right: '-100%'}, 300, function () {
         $(this).css('visibility', 'hidden');
-        $openBtn.focus();
-        //열려진 #mGnb는 닫아준다 - 추가 - 연결해서 하자!
-        $('#mGnb ul li.on').removeClass('on').children('ul').css({visibility: 'hidden', maxHeight: 0});
+        $openBtn.focus(); // 열기 버튼으로 포커스 강제 이동
       });
     });
   });
