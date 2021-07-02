d3bf6aab9a7120483dcbd963dd846021408aea83
Zoe-Kim <thfud333@gmail.com>
2021-07-02 15:06:21 +0900

    1차 대략 완성..
    210702 03:06pm

--- js/main.js
+++ js/main.js
@@ -22,6 +22,18 @@ $(document).ready(function () {
       }
   });
 
+  // pc에서 .bottom_btn 클릭 시 cnt2로 이동
+  $("#pc_bottom_btn").on("click", function() {
+    fullpage_api.moveTo(2);
+  })
+
+  // 태블릿, 모바일에서 .bottom_btn 클릭 시 cnt2로 이동
+  $("#m_bottom_btn").on("click", function() {
+    $('html, body').stop().animate({
+        scrollTop: $('#cnt2').offset().top
+    }, 600);
+  })
+
   // #book_list li a에 마우스와 포커스가 진입하거나 빠져 나갈 때
   const $listEle = $('#book_list li a');
   $listEle.on({
@@ -32,6 +44,16 @@ $(document).ready(function () {
       $(this).removeClass('on').children('.hover_book').stop().slideUp();
     }
   });
-
   
+  //cnt6 .thumb_box에 마우스와 포커스가 진입하거나 빠져 나갈 때(dim 등장처리는 css)
+  const $thumb = $('.thumb_box');
+  $thumb.on({
+    'mouseenter focus': function () {
+      $(this).addClass('on');
+    },
+    'mouseleave blur': function () {
+      $(this).removeClass('on');
+    }
+  });
+
 });
